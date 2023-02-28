c038dae3549d1428c3d8393d278fd09bc928b310
Saksham <saxenasaksham46@gmail.com>
2023-02-28 16:50:13 +0530

    final

--- database/db.js
+++ database/db.js
@@ -1,4 +1,6 @@
 const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+dotenv.config({path:'./config.env'})
 const mongoURI=process.env.DATABASE
 mongoose.set('strictQuery', false)
 mongoose.connect(mongoURI,{
